05b74e5020a8989529a75720805b81daab8cfec6
Vinicius Benedito <beneditoloura@gmail.com>
2022-03-28 08:14:51 -0300

    ensure LocalSavePurchase deletes old ache on save

--- src/domain/usercases/data/usecases/local-save-purchases.spec.ts
+++ src/domain/usercases/data/usecases/local-save-purchases.spec.ts
@@ -1,11 +1,21 @@
 class LocalSavePurchase {
   constructor(private readonly cacheStore: CacheStore) {}
+
+  async save (): Promise<void> {
+    this.cacheStore.delete()
+  }
 }
 
-interface CacheStore {}
+interface CacheStore {
+  delete: () => void
+}
 
 class CacheStoreSpy implements CacheStore {
   deleteCallsCount = 0
+
+  delete (): void {
+    this.deleteCallsCount++
+  }
 }
 
 describe('LocalSavePurchases', () => {
@@ -13,5 +23,12 @@ describe('LocalSavePurchases', () => {
     const cacheStore = new CacheStoreSpy()
     new LocalSavePurchase(cacheStore)
     expect(cacheStore.deleteCallsCount).toBe(0)
+  })
+  
+  test('Should delete old cache on sut.save', async() => {
+    const cacheStore = new CacheStoreSpy()
+    const sut = new LocalSavePurchase(cacheStore)
+    await sut.save()
+    expect(cacheStore.deleteCallsCount).toBe(1)
   })
 })
